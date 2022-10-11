6c0a19596d4378f53ced68f07cf19c299351f93d
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-10-11 18:49:04 +0300

    Create 09. Total salaries and players by country.sql

---
+++ Database Basics MySQL Exam - 9 Feb 2020/09. Total salaries and players by country.sql
@@ -0,0 +1,15 @@
+SELECT 
+c.name,
+count(p.id) AS 'total_count_of_players',
+SUM(p.salary) AS 'total_sum_of_salaries'
+FROM countries AS c
+LEFT JOIN
+towns AS tw ON c.id = tw.country_id
+LEFT JOIN
+stadiums AS s ON s.town_id = tw.id
+LEFT JOIN
+teams AS t ON t.stadium_id = s.id
+LEFT JOIN
+players AS p ON p.team_id = t.id
+GROUP BY c.id
+ORDER BY total_count_of_players DESC,c.name;
