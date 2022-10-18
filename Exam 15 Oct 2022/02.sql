ef34ec4c4a4bd2cbb3ffc88ebc1671dff843b3d8
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-10-18 21:20:23 +0300

    Delete 02.sql

--- Exam 15 Oct 2022/02.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE clients (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    birthdate DATE NOT NULL,
-    card VARCHAR(50),
-    review TEXT
-);
