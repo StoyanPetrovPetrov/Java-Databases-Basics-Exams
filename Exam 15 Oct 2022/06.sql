31221c0741d0d28ef7c5bfd874ab6bf55b762fdd
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-10-18 21:23:28 +0300

    Create 06.sql

---
+++ Exam 15 Oct 2022/06.sql
@@ -0,0 +1,9 @@
+SELECT 
+    c.first_name, c.last_name, c.birthdate, c.review
+FROM
+    clients AS c
+WHERE
+    c.card IS NULL
+     AND YEAR(c.birthdate) BETWEEN 1978 AND 1993
+ORDER BY c.last_name DESC, c.id
+LIMIT 5;
